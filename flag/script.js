e3862cd134c6222f5a395bde92ce1f8cc4f877a7
vasanth0419 <vasanthsalomonv1904@gmail.com>
2023-12-27 23:54:14 +0530

    task update

---
+++ flag/script.js
@@ -0,0 +1,13 @@
+const URL = "https://restcountries.com/v3.1/all";
+const xhr = new XMLHttpRequest();
+xhr.open("GET", URL);
+xhr.send();
+xhr.onload = () => {
+    const infovalue  = JSON.parse(xhr.response) 
+   
+    for (let key in infovalue){
+    
+        console.log(infovalue[key].flags.png)
+    }
+};
+ 
