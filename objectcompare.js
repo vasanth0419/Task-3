b2cb43c17d00a034cc65dc3c228509660202f72a
vasanth0419 <vasanthsalomonv1904@gmail.com>
2023-12-28 15:30:05 +0530

    final update

--- objectcompare.js
+++ objectcompare.js
@@ -1,9 +1,20 @@
 
 let obj1 = {name:"Person 1", age:5};
 let obj2 = {age:5, name:"Person 1"};
+function areObjectsEqual(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
 
-if (obj1.name === obj2.name && obj1.age === obj2.age) {
-    console.log("Objects are equal");
-  } else {
-    console.log("Objects are not equal");
+  if (keys1.length !== keys2.length) {
+    return false;
   }
+
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+console.log(areObjectsEqual(obj1,obj2))
